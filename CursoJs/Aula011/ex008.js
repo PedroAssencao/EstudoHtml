1e65bb2e2d681e802605353a9fa6da60245ee611
PedroAssencao <115711899+PedroAssencao@users.noreply.github.com>
2023-07-11 22:27:04 -0300

    a

---
+++ CursoJs/Aula011/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60.5
+if (vel <= 90) {
+    console.log('Velocidade Permitida na Via, Dirija Sempre com Responsabilidade e Segurança!')
+}else{
+
+    console.log('Velocidade acima do que e permitido na via, por favor desacele o carro por sua segurança!')
+
+}
---
+++ CursoJs/Aula011/ex009.js
@@ -0,0 +1,6 @@
+var a = 50
+
+console.log(`seu carro Esta a ${a}`)
+if (a >= 60) {
+    console.log('Voce esta multado')
+}
