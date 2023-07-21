2b35164245e0cf46bd18dc6447c090344f8b8344
PedroAssencao <115711899+PedroAssencao@users.noreply.github.com>
2023-07-21 17:33:00 -0300

    a

---
+++ CursoJs/Aula013/ex014.js
@@ -0,0 +1,14 @@
+/*
+var c = 0
+while(c < 10){
+    
+    c++
+    console.log(`Seu número é ${c}`)
+}
+*/
+do{
+var d = 1
+d++
+
+console.log(`Seu número é ${d}`)
+}while(d < 9)
---
+++ CursoJs/Aula013/node_fc6e0ca9a831e.tmp.js
@@ -0,0 +1,13 @@
+var c = 0
+while(c < 10){
+    
+    c++
+    console.log(`Seu número é ${c}`)
+}
+
+do{
+var d = 1
+d++
+
+
+}while(d < 9)
