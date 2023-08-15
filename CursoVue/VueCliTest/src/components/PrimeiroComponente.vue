2ee751f4f9e81cbf1f0cc18ac4841d75cc9cd470
PedroAssencao <115711899+PedroAssencao@users.noreply.github.com>
2023-08-14 22:41:55 -0300

    a

---
+++ CursoVue/VueCliTest/src/components/PrimeiroComponente.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+    <h1>Ola vue tudo bem?!</h1>
+    <p>Meu nome é {{ nome }} é meu sobrenome é {{ sobrenome }} se isso esta sendo exibido então e porque deu certo!</p>
+    </div>
+ 
+</template>
+
+<script>
+    export default{
+        name: 'Primeirocomponente',
+        data(){
+            return{
+                nome: 'Pedro',
+                sobrenome: 'Assenção'
+            }
+        }
+    }
+</script>
