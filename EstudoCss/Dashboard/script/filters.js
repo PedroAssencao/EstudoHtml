dd9e3e05723c35aee733dccd49ad9d1c3878ec32
PedroAssencao <115711899+PedroAssencao@users.noreply.github.com>
2024-01-05 16:48:57 -0300

    a

---
+++ EstudoCss/Dashboard/script/filters.js
@@ -0,0 +1,9 @@
+//serch bar code
+$(document).ready(function(){
+    $("#myInput").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("#myTable tr").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+  });
