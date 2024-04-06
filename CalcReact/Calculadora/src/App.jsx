30961059a3afd309a113a2b9badfc76847b918c1
PedroAssencao <115711899+PedroAssencao@users.noreply.github.com>
2024-04-06 11:57:34 -0300

    a

---
+++ CalcReact/Calculadora/src/App.jsx
@@ -0,0 +1,53 @@
+import Button from './components/buttons/index'
+function App() {
+  return (
+    <div className='row g-0 m-0 gap-0 bg-dark'>
+
+      <div className='col-3'>
+        <Button Teste='1' />
+
+      </div><div className='col-3'>
+        <Button Teste='1' />
+
+      </div><div className='col-3'>
+        <Button Teste='1' />
+
+      </div><div className='col-3'>
+        <Button Teste='1' />
+      </div>
+      <div className='col'>
+        <Button Teste='1' />
+
+      </div>
+      <div className='col'>
+        <Button Teste='1' />
+
+      </div>
+      <div className='col'>
+        <Button Teste='1' />
+
+      </div>
+      <div className='col'>
+        <Button Teste='1' />
+
+      </div>
+      <div className='col'>
+        <Button Teste='1' />
+
+      </div>
+      <div className='col'>
+        <Button Teste='1' />
+
+      </div>
+      <div className='col'>
+        <Button Teste='1' />
+
+      </div><div className='col'>
+        <Button Teste='1' />
+
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ CalcReact/Calculadora/src/components/buttons/index.jsx
@@ -0,0 +1,9 @@
+import './style.css'
+function Button(index){
+    return(
+        <div className='BtnNumero'>
+            {index.Teste}
+        </div>
+    )
+}
+export default Button;
